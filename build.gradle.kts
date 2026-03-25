4d86f85ba10ef69e43efd43bae4ca175426d8110
Abidur Rahman Nabil <nabilrt51@gmail.com>
2026-03-25 16:42:53 +0600

    version issue fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    //noinspection NewerVersionAvailable
+    id("org.jetbrains.kotlin.android") version "1.7.0" apply false
 }
