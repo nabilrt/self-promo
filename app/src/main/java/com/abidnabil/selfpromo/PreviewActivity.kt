4d15c0261e474f3bfe99f45693669db1df1ca623
Abidur Rahman Nabil <nabilrt51@gmail.com>
2026-03-26 22:59:59 +0600

    message intent with string resources conversion

--- app/src/main/java/com/abidnabil/selfpromo/PreviewActivity.kt
+++ app/src/main/java/com/abidnabil/selfpromo/PreviewActivity.kt
@@ -1,19 +1,34 @@
 package com.abidnabil.selfpromo
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.net.toUri
 
 class PreviewActivity : AppCompatActivity() {
+
+    private lateinit var message: Message
+    private lateinit var messagePreviewText: String
     var previewMessageTextView: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
-        val message = intent.getSerializableExtra("Message") as Message
-        val messagePreviewText = """
+        displayMessage()
+        setupButton()
+
+
+    }
+
+    private fun displayMessage() {
+        message = intent.getSerializableExtra("Message") as Message
+
+        messagePreviewText = """
             Hi ${message.contactName},
             
             My name is ${message.myDisplayName} and I am ${message.getFullJobDescription()}.
@@ -25,8 +40,19 @@ class PreviewActivity : AppCompatActivity() {
         """.trimIndent()
 
         previewMessageTextView = findViewById(R.id.text_view_message)
-        previewMessageTextView?.setText(messagePreviewText);
-
+        previewMessageTextView?.text = messagePreviewText
 
     }
+
+    private fun setupButton() {
+        val previewButton: Button = findViewById(R.id.button_send_message);
+        previewButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = "smsto: ${message.contactNumber}".toUri()
+                putExtra("sms_body", messagePreviewText)
+            }
+            startActivity(intent)
+        }
+    }
+
 }
